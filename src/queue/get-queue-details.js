81e00157f9edc73ec13fa858c48482b7db210bb2
ersoma <info@somaerdelyi.net>
2020-12-30 20:12:49 +0100

    Implement getQueueDetails middleware

--- src/queue/get-queue-details.js
+++ src/queue/get-queue-details.js
@@ -4,7 +4,11 @@ const Queue = require('bullmq').Queue;
 
 const ParameterError = require('../parameter-error');
 
-module.exports = (queues) => {
+module.exports = (
+  queues, {
+    getQueue = (ctx, queues) => queues.find(q => ctx.params.queueName === q.name),
+    storeResult = (ctx, result) => ctx.state.bullMqAdmin.queueDetails = result
+  } = {}) => {
   if (!queues) {
     throw new ParameterError('queues parameter is required');
   }
@@ -17,7 +21,31 @@ module.exports = (queues) => {
     throw new ParameterError('items in the queues parameter must be BullMQ Queues');
   }
 
-  return () => {
+  if (getQueue instanceof Function === false) {
+    throw new ParameterError('getQueue parameter must be a function');
+  }
 
+  if (storeResult instanceof Function === false) {
+    throw new ParameterError('storeResult parameter must be a function');
+  }
+
+  return async (ctx, next) => {
+    const queue = getQueue(ctx, queues);
+    if (queue instanceof Queue === false) {
+      throw new ParameterError('queue not found');
+    }
+
+    const result = {
+      name: queue.name,
+      isPaused: await queue.isPaused(),
+      activeCount: await queue.getActiveCount(),
+      completedCount: await queue.getCompletedCount(),
+      delayedCount: await queue.getDelayedCount(),
+      failedCount: await queue.getFailedCount(),
+      waitingCount: await queue.getWaitingCount()
+    };
+
+    storeResult(ctx, result);
+    await next();
   };
 };
--- test/queue/get-queue-details.spec.js
+++ test/queue/get-queue-details.spec.js
@@ -1,10 +1,44 @@
 'use strict';
 
-const ParameterError = require('../../src/parameter-error');
+const Queue = require('bullmq').Queue;
 
+const { expect } = require('chai');
+const ParameterError = require('../../src/parameter-error');
 const middlewareFactory = require('../../src/queue/get-queue-details');
 
 describe('getQueueDetailsMiddleware', () => {
+
+  const stubQueueGetters = queue => {
+    global.sandbox.stub(queue, 'isPaused').resolves(true);
+    global.sandbox.stub(queue, 'getActiveCount').resolves(1);
+    global.sandbox.stub(queue, 'getCompletedCount').resolves(2);
+    global.sandbox.stub(queue, 'getDelayedCount').resolves(3);
+    global.sandbox.stub(queue, 'getFailedCount').resolves(4);
+    global.sandbox.stub(queue, 'getWaitingCount').resolves(5);
+  };
+
+  const expectedQueueDetails = {
+    name: 'test',
+    isPaused: true,
+    activeCount: 1,
+    completedCount: 2,
+    delayedCount: 3,
+    failedCount: 4,
+    waitingCount: 5
+  };
+
+  let fakeNext;
+  beforeEach(() => {
+    fakeNext = global.sandbox.stub();
+  });
+
+  let queue;
+  before(() => {
+    queue = new Queue('test');
+  });
+  after(() => {
+    queue.close();
+  });
 
   describe('works as a Koa middleware', () => {
     it('should export a factory function', () => {
@@ -31,5 +65,133 @@ describe('getQueueDetailsMiddleware', () => {
       expect(shouldThrow).to.throw(ParameterError, 'items in the queues parameter must be BullMQ Queues');
     });
 
+    it('should throw an error if optional getQueue parameter is not a function', () => {
+      const shouldThrow = () => middlewareFactory([], {
+        getQueue: 'not a function'
+      });
+      expect(shouldThrow).to.throw(ParameterError, 'getQueue parameter must be a function');
+    });
+
+    it('should throw an error if optional storeResult parameter is not a function', () => {
+      const shouldThrow = () => middlewareFactory([], {
+        storeResult: 'not a function'
+      });
+      expect(shouldThrow).to.throw(ParameterError, 'storeResult parameter must be a function');
+    });
+  });
+
+  describe('works with default parameters', () => {
+
+    it('should throw error if queue is not found', async () => {
+      const middleware = middlewareFactory([queue]);
+      const fakeCtx = {
+        params: {
+          queueName: 'not-test'
+        }
+      };
+
+      expect(middleware(fakeCtx, null)).to.be.rejectedWith(ParameterError, 'queue not found');
+    });
+
+    it('should call next if queue is found', async () => {
+      const middleware = middlewareFactory([queue]);
+
+      const fakeCtx = {
+        params: {
+          queueName: 'test'
+        },
+        state: {
+          bullMqAdmin: {}
+        }
+      };
+
+      await middleware(fakeCtx, fakeNext);
+
+      expect(fakeNext).to.be.calledOnce;
+    });
+
+    it('should call next after storeResult is called', async () => {
+      const fakeStoreResult = global.sandbox.stub();
+      const middleware = middlewareFactory([queue], { storeResult: fakeStoreResult });
+      const fakeCtx = {
+        params: {
+          queueName: 'test'
+        }
+      };
+
+      await middleware(fakeCtx, fakeNext);
+
+      expect(fakeStoreResult).to.be.calledBefore(fakeNext);
+    });
+
+    it('should set the details in default location', async () => {
+      const middleware = middlewareFactory([queue]);
+      stubQueueGetters(queue);
+      const fakeCtx = {
+        params: {
+          queueName: 'test'
+        },
+        state: {
+          bullMqAdmin: {}
+        }
+      };
+
+      await middleware(fakeCtx, fakeNext);
+
+      expect(fakeCtx.state.bullMqAdmin.queueDetails).to.be.eql(expectedQueueDetails);
+    });
+  });
+
+  describe('works with custom getQueue parameter', () => {
+
+    it('should throw an error if getQueue does not return a BullMQ queue instance', async () => {
+      const middleware = middlewareFactory([queue], {
+        getQueue: () => {}
+      });
+      const fakeCtx = {
+        params: {
+          queueName: queue.name
+        }
+      };
+
+      expect(middleware(fakeCtx, null)).to.be.rejectedWith(ParameterError, 'queue not found');
+    });
+
+    it('should set the details in default location', async () => {
+      const middleware = middlewareFactory([queue], {
+        getQueue: (ctx, queues) => queues.find(q => ctx.state.queueCustomName === q.name)
+      });
+      stubQueueGetters(queue);
+      const fakeCtx = {
+        state: {
+          queueCustomName: 'test',
+          bullMqAdmin: {}
+        }
+      };
+
+      await middleware(fakeCtx, fakeNext);
+
+      expect(fakeCtx.state.bullMqAdmin.queueDetails).to.be.eql(expectedQueueDetails);
+    });
+  });
+
+  describe('works with custom storeResult parameter', () => {
+
+    it('should set the details in custom location', async () => {
+      const middleware = middlewareFactory([queue], {
+        storeResult: (ctx, result) => ctx.state.customQueueDetails = result
+      });
+      stubQueueGetters(queue);
+      const fakeCtx = {
+        params: {
+          queueName: 'test'
+        },
+        state: {}
+      };
+
+      await middleware(fakeCtx, fakeNext);
+
+      expect(fakeCtx.state.customQueueDetails).to.be.eql(expectedQueueDetails);
+    });
   });
 });
